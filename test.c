4ab5506887238137e95a40f2fa205ce2bb8f5aa8
Eleazarcheck <279243884+Eleazarcheck@users.noreply.github.com>
2026-05-01 17:26:51 +0300

    delete 'all'

--- test.c
+++ test.c
@@ -11,7 +11,6 @@ if (scanf("%4u",&a) !=1) {
 
 printf ("%s",(a%3000 >=0 && a%3000<=999) ? (a / 3000 == 1 ? "yes" : "no") : "no");
 printf("Hello world");
-printf("Hello all world");
 
     return 0;
 }
